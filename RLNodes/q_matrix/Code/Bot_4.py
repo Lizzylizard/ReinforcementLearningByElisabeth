470216acb82bca18902210108d3f8d366e98bd9b
Elisabeth Milde <elisabeth.milde@informatik.hs-fulda.de>
2020-10-16 15:03:43 +0200

    updated file path

--- RLNodes/q_matrix/Code/Bot_4.py
+++ RLNodes/q_matrix/Code/Bot_4.py
@@ -124,7 +124,7 @@ class Bot:
     try:
       # open correct file
       f = open(
-        "/home/user/workspace/ReinforcementLearningByElisabeth/RLNodes/q_matrix/Q-Matrix-Records.txt",
+        "/home/user/workspace/src/ReinforcementLearningByElisabeth/RLNodes/q_matrix/Q-Matrix-Records.txt",
         "a")
       # f = open("../Q_Matrix/Q-Matrix-Records.txt", "a")
 
